d9c61fd3a97a6834997618e00f562d22dd33df89
youkaixing <youkaixing@78dk.com>
2018-01-17 00:30:59 +0800

    属性监听测试

---
+++ src/avalon/computed.js
@@ -0,0 +1,78 @@
+var vm = avalon.define({
+    $id: "duplex1",
+    data: [{checked: false},  {checked: false},{checked: false},{checked: false},  {checked: false},{checked: false},{checked: false},  {checked: false},{checked: false}],
+    allchecked: false,
+    selectedArr : [],
+    $computed : {
+        selectedArr : function () {
+            var tempArr = [];
+
+            this.data.forEach(function (item) {
+
+                if(item.checked){
+                    tempArr.push(1);
+                }
+            })
+            return tempArr;
+        }
+    },
+    checkAll: function (e) {
+        var checked = e.target.checked
+        vm.data.forEach(function (el) {
+            el.checked = checked
+        })
+    },
+    checkOne: function (el) {
+        // var checked = e.target.checked
+        el.checked = !el.checked;
+        // this.data.set(index,{checked: !el.checked});
+        console.log(this.data.$model);
+        // if (checked === false) {
+        //     vm.allchecked = false
+        // } else {//avalon已经为数组添加了ecma262v5的一些新方法
+        //     vm.allchecked = vm.data.every(function (el) {
+        //         return el.checked
+        //     })
+        // }
+    }
+})
+
+avalon.scan(document.body)
+
+
+// var app = new Vue({
+//     el: '#app',
+//     data: function(){
+//         return {
+//             optData: [{checked: false},  {checked: false},{checked: false}]
+//         }
+//
+//     },
+//     computed : {
+//         selectedArr : function () {
+//             var tempArr = [];
+//             this.optData.forEach(function (item) {
+//
+//                 if(item.checked){
+//                     tempArr.push(1);
+//                 }
+//             })
+//             return tempArr;
+//         }
+//     },
+//
+//     methods : {
+//         checkOne: function (el) {
+//         // var checked = e.target.checked
+//         el.checked = !el.checked;
+//         // this.data.set(index,{checked: !el.checked});
+//         // if (checked === false) {
+//         //     vm.allchecked = false
+//         // } else {//avalon已经为数组添加了ecma262v5的一些新方法
+//         //     vm.allchecked = vm.data.every(function (el) {
+//         //         return el.checked
+//         //     })
+//         // }
+//     }
+//     }
+// })
